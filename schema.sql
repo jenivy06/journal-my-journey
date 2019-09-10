52f0ec95b01acb64b52e00f0e697a49acc1da804
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-09-10 12:28:53 -0600

    trying to deploy

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the journal database if it exists currently --
-DROP DATABASE IF EXISTS journal_db;
+DROP DATABASE IF EXISTS y723273zs7wft6hn;
 -- Creates the journal database --
-CREATE DATABASE journal_db;
+CREATE DATABASE y723273zs7wft6hn;
