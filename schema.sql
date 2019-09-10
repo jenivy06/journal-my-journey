8e8ce087777fe72ea778f24309d3be254d150807
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-09-10 10:55:49 -0600

    updated database name

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Drops the journey database if it exists currently --
-DROP DATABASE IF EXISTS journey_db;
--- Creates the journey database --
-CREATE DATABASE journey_db;
+-- Drops the journal database if it exists currently --
+DROP DATABASE IF EXISTS journal_db;
+-- Creates the journal database --
+CREATE DATABASE journal_db;
